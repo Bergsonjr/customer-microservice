507fe7e393957fa57214131a84bd522a9d3306f8
Bergson Jr <44247346+Bergsonjr@users.noreply.github.com>
2023-06-07 18:25:20 -0300

    clean repo

--- stone-customer-api/src/customer/dto/create-customer.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { Customer } from './customer.dto';
-
-export class CreateCustomer extends OmitType(Customer, ['id'] as const) {}
--- stone-customer-api/src/customer/dto/update-customer.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { Customer } from './customer.dto';
-
-export class UpdateCustomer extends PartialType(Customer) {}
